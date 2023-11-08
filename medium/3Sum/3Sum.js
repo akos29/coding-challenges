ce1d503e5bd9cce335f13f160b838614184eb87f
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-11-08 11:26:18 +0200

    Add solution 2

--- medium/3Sum/3Sum.js
+++ medium/3Sum/3Sum.js
@@ -3,15 +3,34 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    const numSet = new Set();
-    numSet.add(nums);
-
     const triplets = []; 
-   numSet.forEach((value) => {
-        console.log('element',value)
-   
-    })
+ nums.forEach((num, index) => {
+    
+    const target = 0 - num;
+    const numSet = new Set();
+    numSet.add(num);
+    const pairs = twoSum(nums, target, numSet); 
+    if(pairs.length > 0) {
+        triplets.push(...pairs)
+    }
+    });
+    return triplets;
 };
+
+var twoSum = function(nums, target, numSet) {
+    const pairs = [];
+    const pairSet = new Set();
+    nums.forEach((num, index) => {
+        if(numSet.has(num)) return;
+        const diff = target - num;
+        if(pairSet.has(diff)) {
+            pairs.push([num, diff, 0 - target]);
+        } else {
+            pairSet.add(num);
+        }
+    });
+    return pairs;
+}
 
 
 const nums = [-1,0,1,2,-1,-4]
